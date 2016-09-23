6e57041a366a10fb8cb11c23152784171bb31af1
jerryayala64 <ayalajerry46@gmail.com>
2016-09-23 09:20:56 -0700

    Add files via upload

---
+++ Text1.java
@@ -0,0 +1,27 @@
+//save this file as Text1.java
+//javac Text1.java
+
+
+class Text1 {
+    public static void main (String a[]) {
+			int i,ci; // integer i
+			char c; // charater c
+			
+	i= 7;
+	c = 'A';
+	String s = "throw me a bone";		
+	System.out.println("-------------------------");
+	System.out.println(i);
+	System.out.println(c);
+	System.out.println(s);
+		System.out.println("-------------------------");
+	//do some math on the var
+	i = i% 7;
+	ci = (int)c % 7;
+	
+	System.out.println("-------------------------");
+	System.out.println(i);
+	System.out.println(ci);
+
+    }
+}
