2219ee1ae0af44d86fb010d8f812b33621b235ae
jerryayala64 <ayalajerry46@gmail.com>
2016-10-07 08:53:54 -0700

    Update Input.java

--- Input.java
+++ Input.java
@@ -1,21 +1,29 @@
 class Input {
 	public static void main (String a[]){
 	String input1, input2, input3, input4, input5;
-	int sum;
-		input1 = a[0]; input2 = a[1]; input3 = a[2]; input4 = a[3]; input5 = a[4];
-		int b = Integer.parseInt(input1);
-			int c = Integer.parseInt(input2);
-				int d = Integer.parseInt(input3);
-					int e = Integer.parseInt(input4);
-						int f = Integer.parseInt(input5);
-		System.out.println(input1+input2+input3+input4+input5);
-		sum = b+c+d+e+f;
-		int factorial = b*c*d*e*f;
-		int diff = b-c-d-e-f;
-		int div = b/c/d/e/f;
-		System.out.println("sum:"+sum);
-		System.out.println("diff:"+diff);
-		System.out.println("factorial:"+factorial);
-		System.out.println("div:"+div);
+	int sum, pro, diff, div;
+	int ai, bi, ci, di, ei;
+	ai = Integer.parseInt(a[0]);	
+	bi = Integer.parseInt(a[1]);
+	ci = Integer.parseInt(a[2]);
+	di = Integer.parseInt(a[3]);
+	ei = Integer.parseInt(a[4]);	
+		
+	System.out.print(ai+" ");	
+	System.out.print(bi+" ");
+	System.out.print(ci+" ");
+	System.out.print(di+" ");
+	System.out.print(ei+" ");	
+		
+		sum = ai + bi + ci + di + ei;
+		pro = ai * bi * ci * di * ei;
+		diff = -(ai) - bi - ci - di - ei;
+		div = ai / bi / ci / di / ei;
+		
+		System.out.println("sum="+sum);
+		System.out.println("diff="+diff);
+		System.out.println("pro="+pro);
+		System.out.println("div="+div);
+		System.out.println("\n");
 	}
 }
