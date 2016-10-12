8d36c6cc76abca3d4fc09d05d1ce4203f56b9585
jerryayala64 <ayalajerry46@gmail.com>
2016-10-12 08:39:58 -0700

    Create Array1.java

---
+++ Array1.java
@@ -0,0 +1,29 @@
+import java.until.Arrays;
+class Array1 (
+  public static void main (String a[]){
+    int i;
+    int[] ia ;ia = new int[21];
+    
+      ia[0] = -1;
+      ia[1] =  0;
+      ia[2] = -1;
+      ia[3] =  1;
+      ia[4] =  0;
+      ia[5] =  1;
+      ia[6] = -1;
+      ia[7] =  1;
+      ia[8] = -1;
+      ia[9] =  0;
+      
+      System.out.println("Array unsorted\n");
+      for (i = 0; i <=11; i++){
+        System.out.print(ia[i]+" ");
+       }
+       System.out.print("\nArray sorted\n");
+       Array.sort(ia);
+       for (i = 0; i <=11; i++){
+        System.out.print(ia[i]+" ");
+       }
+       System.out.println();
+    }
+}
